3195a0f1c5d8b4055cf1206af6513c288e229c54
jihoo <jihoop0620@gmail.com>
2026-09-16 10:04:30 +0900

    impl: data Term

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+type Var = Int
+type Universe = Int
+type Name = String
+
+data Term
+    = TVar Var
+    | TUniverse Universe
+    | Pi Name Term Term
+    | Lam Name Term Term
+    | App Term Term
+    deriving (Eq, Show)
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
