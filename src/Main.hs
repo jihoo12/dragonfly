932a611aad1d1eba7171e03e408d849366b76d91
jihoo <jihoop0620@gmail.com>
2026-09-16 10:19:50 +0900

    identity and identityType

--- src/Main.hs
+++ src/Main.hs
@@ -12,5 +12,19 @@ data Term
     | App Term Term
     deriving (Eq, Show)
 
+identity :: Term
+identity = 
+    Lam "A" (TUniverse 0)
+        (Lam "x" (TVar 0)
+            (TVar 0))
+
+identityType :: Term
+identityType = 
+    Pi "A" (TUniverse 0)
+        (Pi "x" (TVar 0)
+            (TVar 1))
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    print identity
+    print identityType
