67d0a9938f1214d3687cac271704776da5f58de6
jihoo <jihoop0620@gmail.com>
2026-09-16 10:36:35 +0900

    shift

--- src/Main.hs
+++ src/Main.hs
@@ -24,7 +24,37 @@ identityType =
         (Pi "x" (TVar 0)
             (TVar 1))
 
+shift :: Int -> Int -> Term -> Term
+shift d cutoff (TVar k)
+    | k >= cutoff = TVar (k + d)
+    | otherwise = TVar k
+
+shift _ _ (TUniverse u) = 
+    TUniverse u
+
+shift d cutoff (Pi name domain codomain) =
+    Pi name
+        (shift d cutoff domain)
+        (shift d (cutoff + 1) codomain)
+
+shift d cutoff (Lam name ty body) = 
+    Lam name 
+        (shift d cutoff ty)
+        (shift d (cutoff + 1) body)
+
+shift d cutoff (App f x) = 
+    App
+        (shift d cutoff f)
+        (shift d cutoff x)
+
+
 main :: IO ()
 main = do
+    print "identity and identityType"
     print identity
     print identityType
+    print "shift"
+    print $ shift 1 0 (TVar 0)
+    print $ 
+        shift 1 0 $ 
+            Lam "x" (TUniverse 0) (TVar 0)
