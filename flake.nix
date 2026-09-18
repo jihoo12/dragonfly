4ccebe8c589d5f4d99ceeca76b5069b5466e41f1
jihoo <jihoop0620@gmail.com>
2026-09-18 13:37:45 +0900

    first learning-kernel milestone

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Haskell development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          ghc
+          cabal-install
+          haskell-language-server
+          hlint
+          ormolu
+        ];
+      };
+    };
+}
